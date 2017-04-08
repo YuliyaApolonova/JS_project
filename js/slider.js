579865a014588f4de1872159b956fc68981deb3a
YuliyaApolonova <yuliyaapl@mail.ru>
2017-04-08 13:22:47 +0300

    +slider with timeout

--- js/slider.js
+++ js/slider.js
@@ -8,28 +8,63 @@ var slide1 = document.getElementById('slide1');
 var slide2 = document.getElementById('slide2');
 var slide3 = document.getElementById('slide3');
 var activeLink = 'slide1';
+// var sliderContent = document.getElementById('slider-content');
+// sliderContent.style.width = document.documentElement.clientWidth+'px';
+// alert(sliderContent.style.width);
+var timer = setInterval(nextSlideHandler, 3000);//слайд меняется на следующий каждые 3 с
 
-   // btnPrev.addEventListener('click', prevSlideHandler, false);
+   btnPrev.addEventListener('click', prevSlideHandler, false);
    btnNext.addEventListener('click', nextSlideHandler, false);
+   btnNext.addEventListener('click', clearInt, false);
+   btnPrev.addEventListener('click', clearInt, false);
+
+   function clearInt(){
+      clearInterval(timer);
+      setTimeout(f, 5000);
+      function f(){//получается, что эта функция обрабатывается в любом случае
+      timer = setInterval(nextSlideHandler, 3000);
+      }
+   }
 
    function nextSlideHandler(){
+
       switch(activeLink){
          case 'slide1':
             activeLink = 'slide2';
             var position = slide2.getAttribute("data-pos");
-            alert(position);
             containerSlider.style.left = position;
             break;
+         case 'slide2':
+            activeLink = 'slide3';
+            var position = slide3.getAttribute("data-pos");
+            containerSlider.style.left = position;
+            break;
+         case 'slide3':
+            activeLink = 'slide1';
+            var position = slide1.getAttribute("data-pos");
+            containerSlider.style.left = position;
       }
-}
 
-//    setClickedItem(){
-//    function changePosition(link) {
-//       link.classList.add("active");
-//       var slide = document.getElementById('slide1');
-//       var position = slide.getAttribute("data-pos");
-//       containerSlider.style.left = position;
-//    }
-// }
+   }
 
-//знать, какой слайд активен. В зависимости от того, какой активен, передаватьименно его в переменную slide
+   function prevSlideHandler(){
+      switch(activeLink){
+         case 'slide1':
+            activeLink = 'slide3';
+            var position = slide3.getAttribute("data-pos");
+            containerSlider.style.left = position;
+            break;
+         case 'slide3':
+            activeLink = 'slide2';
+            var position = slide2.getAttribute("data-pos");
+            containerSlider.style.left = position;
+            break;
+         case 'slide2':
+            activeLink = 'slide1';
+            var position = slide1.getAttribute("data-pos");
+            containerSlider.style.left = position;
+            break;
+
+      }
+   }
+
